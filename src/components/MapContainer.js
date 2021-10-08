caadc176bf06abc52c795c815187bdef90353a65
Zack Borges <zackartborges@gmail.com>
2021-10-08 15:38:46 -0400

    adds componentDidMount method and gets data from api successfully fetched

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -1,9 +1,25 @@
 import React, { Component } from "react";
 import { Map, GoogleApiWrapper } from "google-maps-react";
 export class MapContainer extends Component {
+  constructor() {
+    super();
+    this.state = {
+      cars: [],
+    };
+  }
+  componentDidMount() {
+    fetch("https://615f71edf7254d001706813e.mockapi.io/api/cars")
+      .then((res) => res.json())
+      .then((result) => {
+        this.setState({
+          cars: result,
+        });
+        console.log(this.state.cars);
+      });
+  }
   render() {
     return (
-      <Map google={this.props.google} zoom={8} style={mapStyles} initialCenter={{ lat: 33.9519, lng: -83.3576 }} />
+      <Map google={this.props.google} zoom={15} style={mapStyles} initialCenter={{ lat: 33.9519, lng: -83.3576 }} />
     );
   }
 }
